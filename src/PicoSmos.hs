127c33cdf768c45df73807130f66344af0a20374
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-09-25 16:04:02 +0200

    Text for cursor names

--- src/PicoSmos.hs
+++ src/PicoSmos.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module PicoSmos
     ( picoSmos
     ) where
@@ -5,6 +6,7 @@ module PicoSmos
 import Data.Maybe
 
 import qualified Data.Text as T
+import Data.Text (Text)
 import qualified Data.Text.IO as T
 import System.Directory
 import System.Environment
@@ -14,9 +16,9 @@ import Cursor.Text
 
 import Brick as Brick
 import Brick.Main as Brick
-import Brick.Widgets.Core as Brick
 import Brick.Widgets.Border as Brick
 import Brick.Widgets.Center as Brick
+import Brick.Widgets.Core as Brick
 
 import Graphics.Vty.Attributes as Vty
 import Graphics.Vty.Input.Events as Vty
@@ -35,7 +37,7 @@ picoSmos = do
             tc' <- Brick.defaultMain picoSmosApp tc
             T.writeFile file $ rebuildTextCursor tc'
 
-picoSmosApp :: App TextCursor e String
+picoSmosApp :: App TextCursor e Text
 picoSmosApp =
     App
     { appDraw = draw
@@ -45,7 +47,7 @@ picoSmosApp =
     , appAttrMap = const $ attrMap Vty.defAttr []
     }
 
-draw :: TextCursor -> [Widget String]
+draw :: TextCursor -> [Widget Text]
 draw tc =
     [ centerLayer $
       border $
@@ -54,8 +56,7 @@ draw tc =
       txtWrap (rebuildTextCursor tc)
     ]
 
-handleEvent ::
-       TextCursor -> BrickEvent String e -> EventM String (Next TextCursor)
+handleEvent :: TextCursor -> BrickEvent Text e -> EventM Text (Next TextCursor)
 handleEvent tc e =
     case e of
         VtyEvent ve ->
